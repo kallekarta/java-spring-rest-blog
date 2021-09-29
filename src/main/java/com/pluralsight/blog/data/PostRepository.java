e46daf6a90cfbdeb329167300ff8753c7ad6d299
Krister Sundlöf <krister.sundlof@sogeti.se>
2021-09-29 10:46:00 +0200

    Module2

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,14 +1,20 @@
 package com.pluralsight.blog.data;
 
-import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    @RestResource(rel="contains-title", path="containsTitle")
+    public List<Post> findByTitleContaining(String title);
+}
+
+
+
+/*
 @Component
 public class PostRepository {
     public List<Post> findAll() {
@@ -26,4 +32,4 @@ public class PostRepository {
     public Optional<Post> findById(Long id) {
         return null;
     }
-}
+}*/
